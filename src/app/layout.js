d96245ba1950ff34e108b8af2a5fdb0931165e4d
Farwa Rizvi <farwamehdi3@gmail.com>
2025-04-02 20:48:02 +0200

    now galleri is working ,tailwind added

--- src/app/layout.js
+++ src/app/layout.js
@@ -14,7 +14,9 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={poppins.className}>{children}</body>
+      <body suppressHydrationWarning className={poppins.className}>
+        {children}
+      </body>
     </html>
   );
 }
